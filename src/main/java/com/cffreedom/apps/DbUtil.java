e7f1779935320e84fa9278162cf7afe669f6932d
markjacobsen <mark@markjacobsen.net>
2013-06-26 07:43:47 -0400

    Refactoring

--- src/main/java/com/cffreedom/apps/DbUtil.java
+++ src/main/java/com/cffreedom/apps/DbUtil.java
@@ -37,7 +37,9 @@ public class DbUtil
 	private final static String SCRIPT_HIST = SystemUtils.getMyCFConfigDir() + SystemUtils.getPathSeparator() + "dbutils.script.hist";
 	private final static String SQL_HIST = SystemUtils.getMyCFConfigDir() + SystemUtils.getPathSeparator() + "dbutils.sql.hist";
 	private final LoggerUtil logger = new LoggerUtil(LoggerUtil.FAMILY_UTIL, this.getClass().getPackage().getName() + "." + this.getClass().getSimpleName());
-	DbConnManager dcm = null;
+	private DbConnManager dcm = null;
+	private String lastConnKey = null;
+	private String lastMenuChoice = null;
 	
 	public DbUtil() throws DbException
 	{
@@ -63,8 +65,6 @@ public class DbUtil
 	public void run()
 	{
 		final String METHOD = "run";
-		String lastMenuChoice = null;
-		String lastConnKey = null;
 		
 		try
 		{
@@ -78,165 +78,45 @@ public class DbUtil
 			
 			while (foundMenuItem == true)
 			{
-				lastMenuChoice = menu(lastMenuChoice);
+				this.lastMenuChoice = menu(this.lastMenuChoice);
 				
 				Utils.output("");
-				if (lastMenuChoice.equalsIgnoreCase("1") == true)
+				if (this.lastMenuChoice.equalsIgnoreCase("1") == true)
 				{
 					// Run DB Connection Manager
 					this.dcm.run();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("2") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("2") == true)
 				{
-					// Run SQL
-					this.dcm.printKeys();
-					Utils.output("");
-					
-					String key = Utils.prompt("Key", lastConnKey);
-					if (this.dcm.keyExists(key) == true)
-					{
-						lastConnKey = key;
-						Connection conn = this.dcm.getConnection(key);
-						
-						if (conn != null)
-						{
-							Utils.output("SQL (end with ;)");
-							Utils.output("==========================");
-							String sql = Utils.promptBare().trim();
-							while (Utils.lastChar(sql).equalsIgnoreCase(";") == false)
-							{
-								sql += " " + Utils.promptBare().trim();
-							}
-							String sqlWoTerm = sql.substring(0, sql.length() - 1);
-							boolean success = DbUtils.runSql(conn, sqlWoTerm);
-							conn.close();
-							
-							if (success == true)
-							{
-								addToSqlHist(sql);
-							}
-						}
-					}
-					else
-					{
-						Utils.output("ERROR: Unknown connection: " + key);
-					}
+					this.menuRunSql();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("3") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("3") == true)
 				{
-					// Run SQL Script
-					this.dcm.printKeys();
-					Utils.output("");
-					
-					String key = Utils.prompt("Key", lastConnKey);
-					if (this.dcm.keyExists(key) == true)
-					{
-						lastConnKey = key;
-						Connection conn = this.dcm.getConnection(key);
-						
-						if (conn != null)
-						{
-							String scriptFile = getScriptFile();
-							
-							if (FileUtils.fileExists(scriptFile) == true)
-							{
-								String format = Utils.prompt("Format: XML,CSV,TAB", DbUtils.FORMAT.XML.toString()).toUpperCase();
-								if (DbUtils.validFormat(format) == false) { format = DbUtils.FORMAT.XML.toString(); }
-								int errors = DbUtils.runSqlScript(conn, scriptFile, DbUtils.FORMAT.valueOf(format));
-								if (errors != 0)
-								{
-									Utils.output("There were errors running the script: " + scriptFile);
-								}
-								conn.close();
-						
-								addToScriptHist(scriptFile);
-							}
-							else
-							{
-								logger.logWarn(METHOD, "Script file does NOT exist: " + scriptFile);
-							}
-						}
-					}
-					else
-					{
-						Utils.output("ERROR: Unknown connection: " + key);
-					}
+					this.menuRunSqlScript();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("4") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("4") == true)
 				{
-					// List tables
-					this.dcm.printKeys();
-					Utils.output("");
-					
-					String key = Utils.prompt("Key", lastConnKey);
-					if (this.dcm.keyExists(key) == true)
-					{
-						lastConnKey = key;
-						DbConn dbconn = this.dcm.getDbConnWithUserInfo(key);
-						DbUtils.listTables(dbconn);
-					}
+					this.menuListTables();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("80") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("80") == true)
 				{
-					// Show SQL Hist
-					if (FileUtils.fileExists(DbUtil.SQL_HIST) == true){
-						Utils.output(FileUtils.getFileContents(DbUtil.SQL_HIST));
-					}else{
-						Utils.output("No history yet.");
-					}
+					this.menuShowSqlHist();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("81") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("81") == true)
 				{
-					// Show Script Hist
-					if (FileUtils.fileExists(DbUtil.SCRIPT_HIST) == true){
-						Utils.output(FileUtils.getFileContents(DbUtil.SCRIPT_HIST));
-					}else{
-						Utils.output("No history yet.");
-					}
+					this.menuShowScriptHist();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("90") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("90") == true)
 				{
-					// Delete SQL Hist
-					if (Utils.prompt("Are you sure you want to delete your history?", "N").equalsIgnoreCase("Y") == true)
-					{
-						if (FileUtils.deleteFile(DbUtil.SQL_HIST) == true){
-							Utils.output("Script history deleted successfully");
-						}
-					}
+					this.menuDeleteSqlHist();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("91") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("91") == true)
 				{
-					// Delete Script Hist
-					if (Utils.prompt("Are you sure you want to delete your history?", "N").equalsIgnoreCase("Y") == true)
-					{
-						if (FileUtils.deleteFile(DbUtil.SCRIPT_HIST) == true){
-							Utils.output("Script history deleted successfully");
-						}
-					}
+					this.menuDeleteScriptHist();
 				}
-				else if (lastMenuChoice.equalsIgnoreCase("92") == true)
+				else if (this.lastMenuChoice.equalsIgnoreCase("92") == true)
 				{
-					// Delete missing files from Script Hist
-					if (Utils.prompt("Are you sure you want to remove missing files from your history?", "N").equalsIgnoreCase("Y") == true)
-					{
-						int counter = 0;
-						ArrayList<String> files = FileUtils.getFileLines(SCRIPT_HIST);
-						if (files.size() > 0)
-						{
-							for (String file : files)
-							{
-								if (FileUtils.fileExists(file) == false)
-								{
-									if (FileUtils.stripLinesInFileContaining(SCRIPT_HIST, file) == true)
-									{
-										Utils.output("Removed from hist: " + file);
-										counter++;
-									}
-								}
-							}
-							Utils.output("Removed " + counter + " files from your history");
-						}
-					}
+					this.menuCleanupScriptHistFile();
 				}
 				else
 				{
@@ -275,6 +155,160 @@ public class DbUtil
 		Utils.output("91) Delete Script Hist");
 		Utils.output("92) Delete Missing Scripts from Script Hist");
 		return Utils.prompt("Choice", lastChoice);
+	}
+	
+	private void menuRunSql() throws SQLException
+	{
+		this.dcm.printKeys();
+		Utils.output("");
+		
+		String key = Utils.prompt("Key", this.lastConnKey);
+		if (this.dcm.keyExists(key) == true)
+		{
+			this.lastConnKey = key;
+			Connection conn = this.dcm.getConnection(key);
+			
+			if (conn != null)
+			{
+				Utils.output("SQL (end with ;)");
+				Utils.output("==========================");
+				String sql = Utils.promptBare().trim();
+				while (Utils.lastChar(sql).equalsIgnoreCase(";") == false)
+				{
+					sql += " " + Utils.promptBare().trim();
+				}
+				String sqlWoTerm = sql.substring(0, sql.length() - 1);
+				boolean success = DbUtils.runSql(conn, sqlWoTerm);
+				conn.close();
+				
+				if (success == true)
+				{
+					addToSqlHist(sql);
+				}
+			}
+		}
+		else
+		{
+			Utils.output("ERROR: Unknown connection: " + key);
+		}
+	}
+	
+	private void menuRunSqlScript() throws SQLException
+	{
+		final String METHOD = "menuRunSqlScript";
+		
+		this.dcm.printKeys();
+		Utils.output("");
+		
+		String key = Utils.prompt("Key", this.lastConnKey);
+		if (this.dcm.keyExists(key) == true)
+		{
+			this.lastConnKey = key;
+			Connection conn = this.dcm.getConnection(key);
+			
+			if (conn != null)
+			{
+				String scriptFile = getScriptFile();
+				
+				if (FileUtils.fileExists(scriptFile) == true)
+				{
+					String format = Utils.prompt("Format: XML,CSV,TAB", DbUtils.FORMAT.XML.toString()).toUpperCase();
+					if (DbUtils.validFormat(format) == false) { format = DbUtils.FORMAT.XML.toString(); }
+					int errors = DbUtils.runSqlScript(conn, scriptFile, DbUtils.FORMAT.valueOf(format));
+					if (errors != 0)
+					{
+						Utils.output("There were errors running the script: " + scriptFile);
+					}
+					conn.close();
+			
+					addToScriptHist(scriptFile);
+				}
+				else
+				{
+					logger.logWarn(METHOD, "Script file does NOT exist: " + scriptFile);
+				}
+			}
+		}
+		else
+		{
+			Utils.output("ERROR: Unknown connection: " + key);
+		}
+	}
+	
+	private void menuListTables()
+	{
+		this.dcm.printKeys();
+		Utils.output("");
+		
+		String key = Utils.prompt("Key", this.lastConnKey);
+		if (this.dcm.keyExists(key) == true)
+		{
+			this.lastConnKey = key;
+			DbConn dbconn = this.dcm.getDbConnWithUserInfo(key);
+			DbUtils.listTables(dbconn);
+		}
+	}
+	
+	private void menuShowSqlHist()
+	{
+		if (FileUtils.fileExists(DbUtil.SQL_HIST) == true){
+			Utils.output(FileUtils.getFileContents(DbUtil.SQL_HIST));
+		}else{
+			Utils.output("No history yet.");
+		}
+	}
+	
+	private void menuShowScriptHist()
+	{
+		if (FileUtils.fileExists(DbUtil.SCRIPT_HIST) == true){
+			Utils.output(FileUtils.getFileContents(DbUtil.SCRIPT_HIST));
+		}else{
+			Utils.output("No history yet.");
+		}
+	}
+	
+	private void menuDeleteSqlHist()
+	{
+		if (Utils.prompt("Are you sure you want to delete your history?", "N").equalsIgnoreCase("Y") == true)
+		{
+			if (FileUtils.deleteFile(DbUtil.SQL_HIST) == true){
+				Utils.output("Script history deleted successfully");
+			}
+		}
+	}
+	
+	private void menuDeleteScriptHist()
+	{
+		if (Utils.prompt("Are you sure you want to delete your history?", "N").equalsIgnoreCase("Y") == true)
+		{
+			if (FileUtils.deleteFile(DbUtil.SCRIPT_HIST) == true){
+				Utils.output("Script history deleted successfully");
+			}
+		}
+	}
+	
+	private void menuCleanupScriptHistFile()
+	{
+		if (Utils.prompt("Are you sure you want to remove missing files from your history?", "N").equalsIgnoreCase("Y") == true)
+		{
+			int counter = 0;
+			ArrayList<String> files = FileUtils.getFileLines(SCRIPT_HIST);
+			if (files.size() > 0)
+			{
+				for (String file : files)
+				{
+					if (FileUtils.fileExists(file) == false)
+					{
+						if (FileUtils.stripLinesInFileContaining(SCRIPT_HIST, file) == true)
+						{
+							Utils.output("Removed from hist: " + file);
+							counter++;
+						}
+					}
+				}
+				Utils.output("Removed " + counter + " files from your history");
+			}
+		}
 	}
 	
 	private String getScriptFile()
